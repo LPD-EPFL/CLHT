ad00187a7b992623491e094c05a5ad0e7bbf5e0a
Vasileios Trigonakis <vasileios.trigonakis@epfl.ch>
2013-03-11 19:42:43 +0100

    run_throughput.sh

---
+++ run_throughput.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+read -p "Enter the max number of cores: " NUM_CORES
+
+duration=1000
+buckets="512 16";
+rw="0'1 0.1'0.9";
+
+fill_rate=0.5;
+payload_size=64;
+
+executables=$(ls throughput_*);
+cores=$(seq 1 1 $NUM_CORES);
+
+for bu in $buckets
+do
+    num_elems=$((24*$bu));
+
+    for r in $rw
+    do 
+	u=$(echo $r | cut -d"'" -f1);
+	g=$(echo $r | cut -d"'" -f2);
+	echo "#buckets: $bu / update: $u / get: $g";
+
+	out_dat="data/throughput.b"$bu"_u"$u"_diff_locks.dat"
+
+	printf "#  " | tee $out_dat;
+	for f in $(ls throughput_*)
+	do
+	    prim=$(echo $f | cut -d'_' -f2);
+	    printf "%-11s" $prim | tee $out_dat;
+	done;
+	echo "" | tee $out_dat;
+
+	for c in $cores
+	do
+	    printf "%-3u" $c
+	    for ex in $executables
+	    do
+		p="$bu $c $num_elems $fill_rate $payload_size $duration $u $g";
+		./$ex $p | awk '// { printf "%-11d", $2 }' | tee $out_dat;
+	    done;
+	    echo "" | tee $out_dat;
+	done;
+    done;
+done;
