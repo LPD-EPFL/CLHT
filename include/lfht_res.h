657cbcfc324eace69159c545583485adcc37e39a
Vasileios Trigonakis <vasileios.trigonakis@epfl.ch>
2014-11-27 13:24:35 +0100

    increased the #retries when there is no empty slot to 16

--- include/lfht_res.h
+++ include/lfht_res.h
@@ -35,7 +35,7 @@ extern __thread ssmem_allocator_t* hyht_alloc;
 #define LFHT_PERC_FULL_DOUBLE    15
 #define LFHT_OCCUP_AFTER_RES     40
 #define LFHT_INC_EMERGENCY       2
-#define LFHT_NO_EMPTY_SLOT_TRIES 3
+#define LFHT_NO_EMPTY_SLOT_TRIES 16
 #define LFHT_GC_HT_VERSION_USED(ht) ht_gc_thread_version(ht)
 #define LOAD_FACTOR              0.5
 
